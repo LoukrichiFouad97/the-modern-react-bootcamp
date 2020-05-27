4892970575b2859ebd5ca60bbb677210b5797c54
fouad97 <loukrichi21@gmail.com>
2020-05-27 17:15:59 +0100

    added first section

---
+++ 01-taste-of-react/index.js
@@ -0,0 +1,13 @@
+const root = document.getElementById("root");
+
+class Head extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>test</h1>
+			</div
+		);
+	}
+}
+
+ReactDOM.render(<Head />, root);
